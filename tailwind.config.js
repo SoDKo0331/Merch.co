441130bcbce24549ba68ac37501191cfb4dcb8a7
unknown <ssodko245@gmail.com>
2026-01-31 21:59:46 +0800

    m

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,30 @@ module.exports = {
     "./index.html",
     "./**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "electric-blue": "#2D32FF",
+        "neon-lime": "#C6FF00",
+        "deep-black": "#000000",
+        "stark-white": "#FFFFFF",
+        "hot-pink": "#FF00E5",
+      },
+      fontFamily: {
+        display: ["Archivo Black", "sans-serif"],
+        body: ["Archivo", "sans-serif"],
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
